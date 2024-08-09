50c82e03b32fd5ed2d3078b2e19d95652849e8d5
Dean Dunbar <dean.dunbar@gong.io>
2024-08-08 19:26:27 -0500

    new slideshow format

--- presentation/app.py
+++ presentation/app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 slides = [
     {
         "title": "Diceware- Rolling your own encryption",
-        "content": ["This is the first slide", "You can add bullet points", "And more text"],
+        "content": [
+    "Randomness Rocks: Discover why a little chaos makes your keys super secure.",
+    "Get Rolling: Learn how to craft your own keys with a few rolls of the dice.",
+    "From Dice to Devices: Endless possibilites"
+],
         "background_color": "#041e00",  # Default to a blue color
         "background_image": None,  # Set to a URL or path to an image
     },
@@ -30,9 +34,34 @@ slides = [
     # Add more slides as needed
 ]
 
+
+bitcoin_slides =  [
+    {
+        "title": "Diceware- Rolling your own encryption",
+        "content": [
+    "Randomness Rocks: Discover why a little chaos makes your keys super secure.",
+    "Get Rolling: Learn how to craft your own keys with a few rolls of the dice.",
+    "From Dice to Devices: Endless possibilites"
+],
+        "background_color": "#041e00",  # Default to a blue color
+        "background_image": None,  # Set to a URL or path to an image
+    },
+]
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
 @app.route('/')
 def slideshow():
     return render_template('slide.html', slides=slides)
+
+
+
+@app.route('/bitcoin')
+def bitcoin():
+    return render_template('slide.html', slides=bitcoin_slides)
 
 if __name__ == '__main__':
     app.run(debug=True)
